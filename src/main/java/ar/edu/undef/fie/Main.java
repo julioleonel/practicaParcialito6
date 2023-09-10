2734058aa77c7f703be24fa9b99b6e99de772b3d
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-10 19:42:50 -0300

    subo codigo

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -2,6 +2,23 @@ package ar.edu.undef.fie;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length == 0) {
+            System.out.println("No se proporcionaron palabras como argumentos.");
+            return;
+        }
+
+        for (String palabra : args) {
+            int contadorVocales = 0;
+            palabra = palabra.toLowerCase(); // Convertir la palabra a minúsculas para considerar todas las vocales.
+
+            for (int i = 0; i < palabra.length(); i++) {
+                char caracter = palabra.charAt(i);
+                if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u') {
+                    contadorVocales++;
+                }
+            }
+
+            System.out.println("La palabra '" + palabra + "' tiene " + contadorVocales + " vocales.");
+        }
     }
 }
